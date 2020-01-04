cf48045ddbf2b92938e0477924a4301480b064b0
wcampbell <wcampbell1995@gmail.com>
2020-01-04 21:09:08 +0000

    Intial Commit
    Everything is writen out, just having an issue with sending data to the
    saleae device
    
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

---
+++ examples/example.rs
@@ -0,0 +1,14 @@
+use saleae;
+
+use saleae::Client;
+use std::net::TcpStream;
+
+fn main() {
+    //let mut conn = Client::connect("127.0.0.1:6600").unwrap();
+    let mut conn = Client::new(TcpStream::connect("127.0.0.1:10429").unwrap()).unwrap();
+    //let response0 = conn.get_performance();
+    //println!("get_performance: {}", response0.unwrap());
+
+    let response1 = conn.get_connected_devices();
+    println!("get_command_devices: {}", response1.unwrap());
+}
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+//! Saleae client for Rust
+//!
+//! This crate provides a Rust API for [Saleae](https://www.saleae.com).
+//! The API is based on the documentation provided:
+//! [SaleaeSocketAPI](https://github.com/saleae/SaleaeSocketApi)
+//!
+//! The main entry point for this API is the ['Client'](client/struct.Client.html) struct.
+//!
+//!
+//! # Usage
+//! ```text
+//! [dependencies]
+//! saleae = "*"
+//! ```
+//!
+//! # Example with no error handling
+//! ```rust, no_run
+//! extern crate mpd;
+//!
+//! use mpd::Client;
+//! use std::net::TcpStream;
+//!
+//! # fn main() {
+//! let mut conn = Client::connect("127.0.0.1:6600").unwrap();
+//! conn.set_performance(Options::Performance::Full)
+//! //let devices = conn.get_connected_devices()
+//! //for device in devices {
+//! //    println!(
+//! #}
+
+extern crate anyhow;
+extern crate bufstream;
+
+pub mod client;
+pub use client::Client;
