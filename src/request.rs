7e76e3a600019d13438bcd51a26e4960f7edcb01
wcampbell <wcampbell1995@gmail.com>
2020-01-12 17:59:31 -0500

    Add general function for receiving msgs
    Add functions to just recieve ack, then other to recieve ack and message
    as string
    
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- src/request.rs
+++ src/request.rs
@@ -1,14 +1,27 @@
 //! This module helps create requests to the Saleae Logic software
 //!
-//! The function here parse the input from the API into strings to send into
+//! The function here parse and type check the input from the API into strings to send into
 //! the Saleae socket
+use anyhow::Result;
 
 pub struct Request {}
 
 impl Request {
-    pub fn create_channel_str(v: &[u8]) -> String
+     pub fn prepare_set_active_channels(digital_channels: &[u8], analog_channels: &[u8]) -> Result<String>
+     {
+        /* TODO verify channels being selected? */
+        let d_str: String = format!("digital_channels, {}", Request::create_channel_str(digital_channels)?);
+        let a_str: String = format!("analog_channels, {}", Request::create_channel_str(analog_channels)?);
+
+        Ok(format!("set_active_channels, {}, {}\0", d_str, a_str))
+     }
+}
+
+/// Helper functions
+impl Request {
+    pub fn create_channel_str(v: &[u8]) -> Result<String>
     {
         let s = v.into_iter().map(|a| format!("{}, ", a.to_string())).collect::<String>();
-        s[..s.len() - 2].to_string()
+        Ok(s[..s.len() - 2].to_string())
     }
 }
--- tests/request.rs
+++ tests/request.rs
@@ -1,13 +1,16 @@
 #[cfg(test)]
 mod tests {
     use saleae::request::Request;
+    use anyhow::Result;
+/// Regular function tests
 
+/// Help function tests
     #[test]
     fn test_create_channel_str() {
         let input = [0, 4, 5, 7];
-        assert_eq!("0, 4, 5, 7", Request::create_channel_str(&input));
+        assert_eq!("0, 4, 5, 7", Request::create_channel_str(&input).unwrap());
 
         let input = [1, 2, 3, 5, 8];
-        assert_eq!("1, 2, 3, 5, 8", Request::create_channel_str(&input));
+        assert_eq!("1, 2, 3, 5, 8", Request::create_channel_str(&input).unwrap());
     }
 }
